c6cb2735b590f9bb8bf139cf4a9f9d2ad07cc10b
Max-129 <sadvibe129@gmail.com>
2023-05-08 15:02:11 +0600

    Initial commit

---
+++ app/src/main/java/com/example/a62/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a62
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a62.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/a62/Sheep.kt
@@ -0,0 +1,6 @@
+package com.example.a62
+
+data class Sheep(
+    val image: String,
+    var isSelected: Boolean = false
+) : java.io.Serializable
